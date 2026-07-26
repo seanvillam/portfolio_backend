95f24698a479d22972a94fdc4956bfcd434c5b30
Sean Villamonte <seanpajarillo@gmail.com>
2026-07-25 21:22:23 -0400

    Update MainRouter.jsx

--- portfolio-backend-acb8724da4bf12fc21247c46b9de890687c6bf5c/client/MainRouter.jsx
+++ portfolio-backend-acb8724da4bf12fc21247c46b9de890687c6bf5c/client/MainRouter.jsx
@@ -24,6 +24,18 @@ const MainRouter = () => {
             <Route path="/services" element={<Services />} />
             <Route path="/counter" element={<Counter />} />
             <Route path="/admin" element={<AdminDashboard />} />
+            <Route path="/admin/users" element={<h2>Manage Users</h2>} />
+            <Route path="/admin/users/add" element={<h2>Add Users</h2>} />
+            <Route path="/admin/users/edit:id" element={<h2>Edit Users</h2>} />
+            <Route path="/admin/projects" element={<h2>Manage Projects</h2>} />
+            <Route path="/admin/projects/add" element={<h2>Add Projects</h2>} />
+            <Route path="/admin/projects/edit:id" element={<h2>Edit Projects</h2>} />
+            <Route path="/admin/services" element={<h2>Manage Services</h2>} />
+            <Route path="/admin/services/add" element={<h2>Add Services</h2>} />
+            <Route path="/admin/services/edit:id" element={<h2>Edit Services</h2>} />
+            <Route path="/admin/references" element={<h2>Manage References</h2>} />
+            <Route path="/admin/references/add" element={<h2>Add References</h2>} />
+            <Route path="/admin/references/edit:id" element={<h2>Edit References</h2>} />
         </Routes>
     </div>)
 }
